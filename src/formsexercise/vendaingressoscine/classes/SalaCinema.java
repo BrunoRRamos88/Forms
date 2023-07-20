058146f6aeb4cf27f794d7c97416467763d9ec3a
Bruno R Ramos <bruno.rr.contato@gmail.com>
2023-07-20 14:27:32 -0300

    Atualizado forms

--- src/formsexercise/vendaingressoscine/classes/SalaCinema.java
+++ src/formsexercise/vendaingressoscine/classes/SalaCinema.java
@@ -1,17 +1,10 @@
 package formsexercise.vendaingressoscine.classes;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class SalaCinema {
-    private List<Ingresso> poltronas;
 
 
-    public SalaCinema (){
-        poltronas = new ArrayList<>();
-        for (int i = 1; i <= 100; i++) {
-            Ingresso ingresso = new Ingresso();
-            poltronas.add(ingresso);
-        }
-    }
+
+
 }
+
